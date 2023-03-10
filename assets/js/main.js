83258e129561cf0c5f94280541facc37dfda28dd
cyberfolk <andrea1995ferrari.com>
2023-03-10 14:23:45 +0100

    Implement algoritm fizzbuzz and log to consol

--- assets/js/main.js
+++ assets/js/main.js
@@ -21,3 +21,15 @@
  * Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.*/
 
 console.log("funziona?");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log(`${i} = FizzBuzz`);
+  } else if (i % 3 == 0) {
+    console.log(`${i} = Fizz`);
+  } else if (i % 5 == 0) {
+    console.log(`${i} = Buzz`);
+  } else {
+    console.log(`${i}`);
+  }
+}
