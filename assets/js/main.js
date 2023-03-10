d63e6a36eadc9b444206fb203bcc99660cb102b2
cyberfolk <andrea1995ferrari.com>
2023-03-10 14:10:37 +0100

    Initial commit

---
+++ assets/js/main.js
@@ -0,0 +1,23 @@
+/* Consegna:
+ * Scrivi un programma che stampi in console i numeri da 1 a 100 ma che:
+ *  - per i multipli di 3 stampi “Fizz” al posto del numero
+ *  - per i multipli di 5 stampi “Buzz”
+ *  - per i numeri che sono sia multipli di 3 che di 5 stampi “FizzBuzz”.
+ *
+ * Consigli del giorno:
+ * 1. Scriviamo sempre prima dei commenti in italiano o inglese per capire cosa vogliamo fare
+ * 2. Proviamo ad immaginare le operazioni che vogliamo far svolgere al nostro programma così come lo faremmo "a mano"
+ * 3. Facciamo attenzione all'ordine delle condizioni che usiamo
+ *
+ * BONUS 1:
+ * Crea un container nel DOM , aggiungendo un elemento html con il numero o la stringa corretta da mostrare.
+ * Potete usare vari strumenti per farlo:
+ *  - `append()` oppure
+ *  - `.innerHTML`
+ *  - `.insertAdjacentHTML()`
+ *
+ * BONUS 2:
+ * Applica stili differenti agli elementi aggiunti al DOM nel *BONUS 1*, a seconda che il valore inserito sia un numero, un fizz, un buzz o un fizzbuzz.
+ * Se sei a corto di idee per lo stile, potresti prendere spunto dallo screenshot fornito in consegna.*/
+
+console.log("funziona?");
