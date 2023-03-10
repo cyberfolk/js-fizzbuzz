d3e8ea417cb9bdfa45d8c0e06d556512c912a090
cyberfolk <andrea1995ferrari.com>
2023-03-10 16:09:21 +0100

    After ticket

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,21 +24,17 @@ const rowEl = document.querySelector(".row");
 console.log(rowEl);
 
 for (let i = 1; i <= 100; i++) {
-  let special = "";
+  let special = `${i}`;
   let bgColorCard = "bg-info";
-  let j = i;
   if (i % 3 == 0 && i % 5 == 0) {
     special = "Fizzbuzz";
     bgColorCard = "bg-success";
-    j = "";
   } else if (i % 3 == 0) {
     special = "Fizz";
     bgColorCard = "bg-warning";
-    j = "";
   } else if (i % 5 == 0) {
     special = "Buzz";
     bgColorCard = "bg-danger";
-    j = "";
   }
 
   console.log(`${i} ${special}`);
@@ -54,9 +50,8 @@ for (let i = 1; i <= 100; i++) {
   cardEl.classList.add("card", "p-3");
   console.log(`cardEl: ${cardEl}. classe: ${cardEl.classList}`);
 
-  const colCardEl = colElement.appendChild(cardEl);
-  console.log(`colCardEl: ${colCardEl}`);
+  colElement.appendChild(cardEl);
+  console.log(`colElement: ${colElement}. classe: ${colElement.classList}`);
 
-  console.log(colCardEl);
-  rowEl.append(colCardEl);
+  rowEl.append(colElement);
 }
